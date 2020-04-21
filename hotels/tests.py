477c2a3fd01ebf6201b982b342ad9650b9887a5f
tungpham1029 <tungpham1029@gmail.com>
2020-04-21 22:15:22 +0700

    everything is okie

--- hotels/tests.py
+++ hotels/tests.py
@@ -1,3 +1,10 @@
+from . import views
 from django.test import TestCase
+from django.urls import reverse, resolve
 
-# Create your tests here.
+
+class HomeTests(TestCase):
+    def test_home_view_status_code(self):
+        url = reverse('hotels:home')
+        response = self.client.get(url)
+        self.assertEquals(response.status_code, 200)
